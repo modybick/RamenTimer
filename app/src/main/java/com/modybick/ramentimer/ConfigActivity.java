d1973152e57ac70c8c0d57afe864c3f540e19a5d
ModyBick <haruki.mjp@gmail.com>
2018-11-14 18:10:18 +0900

    初期化実装 ひとまず完成？

--- app/src/main/java/com/modybick/ramentimer/ConfigActivity.java
+++ app/src/main/java/com/modybick/ramentimer/ConfigActivity.java
@@ -1,43 +1,52 @@
 package com.modybick.ramentimer;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 
+@SuppressLint("ExportedPreferenceActivity")
 public class ConfigActivity extends PreferenceActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         PreferenceManager preferenceManager = getPreferenceManager();
         preferenceManager.setSharedPreferencesName(getString(R.string.preference_name));
-
         addPreferencesFromResource(R.xml.preferences);
+
+        //初期化ボタンにクリックリスナー設定
+        Preference pref_ini = findPreference("pref_initialize");
+        pref_ini.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(final Preference preference) {
+                initialize_Alert(savedInstanceState);
+                return true;
+            }
+        });
     }
-    
-    PreferenceScreen preferenceScreen = findPreference("pref_initialize");
-    preferenceScreen.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-        @Override
-        public boolean onPreferenceClick(Prefrence preference) {
-            // 初期化確認ダイアログ表示
-            new AlertDialog.Builder(getActivity())
-                
-                // TODO テキストをresに
-                
-                .setTitle("初期化")
-                .setMessage("設定をインストール時の状態に戻します")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+    private void initialize_Alert(final Bundle savedInstanceState) {
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.pref_initialize))
+                .setMessage(getString(R.string.pref_initialize_text))
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // OKボタンクリック時、設定初期化を実行
-                        SheredPreferences pref = getSharedPreferences(getString(R.string.preference_name), MODE_PRIVATE);
-                        pref.edit().clear();                        
+                        // OK button pressed
+                        SharedPreferences pref = getSharedPreferences(getString(R.string.preference_name), MODE_PRIVATE);
+                        pref.edit().clear().apply();
+                        onCreate(savedInstanceState);
                     }
                 })
-                .setNegativeButton("Cancel", null)
+                .setNegativeButton(getString(R.string.cancel), null)
                 .show();
-            return true;
-        }
+
     }
-    
+
 }
+
--- app/src/main/java/com/modybick/ramentimer/ConfigFragment.java
+++ app/src/main/java/com/modybick/ramentimer/ConfigFragment.java
@@ -1,13 +1,20 @@
 package com.modybick.ramentimer;
 
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.preference.Preference;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Objects;
 
 public class ConfigFragment extends PreferenceFragmentCompat {
 
@@ -22,6 +29,30 @@ public class ConfigFragment extends PreferenceFragmentCompat {
         PreferenceManager preferenceManager = getPreferenceManager();
         preferenceManager.setSharedPreferencesName(getString(R.string.preference_name));
         addPreferencesFromResource(R.xml.preferences);
+
+        //初期化ボタンにクリックリスナー設定
+        Preference pref_ini = findPreference("pref_initialize");
+        pref_ini.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public boolean onPreferenceClick(final Preference preference) {
+                new AlertDialog.Builder(Objects.requireNonNull(getActivity()))
+                        .setTitle(getString(R.string.pref_initialize))
+                        .setMessage(getString(R.string.pref_initialize_text))
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // OK button pressed
+                                //preference.getEditor().clear();
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.cancel), null)
+                        .show();
+
+                return true;
+            }
+        });
+
     }
 
 
